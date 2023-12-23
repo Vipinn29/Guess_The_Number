f096745a1b3f06291fd64ca788bea3fc4631f3c1
Vipinn29 <vipingupta2k4@gmail.com>
2023-12-24 00:24:07 +1300

    Source code

---
+++ Guess_the_no.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void main()
+{
+    int n, guess, nguesses = 1;
+    srand(time(0));
+    n = rand() % 100;
+    // printf("The no. is %d\n", n);
+    printf("Guess the no. between 1 to 100.\n");
+    do
+    {
+        scanf("%d", &guess);
+        if (n < guess)
+        {
+            printf("Lower value please!\n");
+        }
+        else if (n > guess)
+        {
+            printf("Higher value please!\n");
+        }
+        else
+        {
+            printf("Congrats! You guessed it in %d attempts\n", nguesses);
+        }
+        nguesses++;
+    } while (n != guess);
+}
